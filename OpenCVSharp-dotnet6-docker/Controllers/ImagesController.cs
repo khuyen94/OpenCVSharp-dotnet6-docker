5c9ef8e60e56e77aa763309f370184a29abca6b0
Khuyen Tran <khuyen@dws.cloud>
2023-06-01 11:44:05 +0700

    final commit

---
+++ OpenCVSharp-dotnet6-docker/Controllers/ImagesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenCVSharp_dotnet6_docker.Services;
+
+namespace OpenCVSharp_dotnet6_docker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile photo)
+        {
+            try
+            {
+                string uploadFolder = Path.Combine(AppContext.BaseDirectory, "Images");
+                if (!Directory.Exists(uploadFolder))
+                {
+                    Directory.CreateDirectory(uploadFolder);
+                }
+                if (photo == null || photo.Length == 0)
+                {
+                    return Content("File not selected");
+                }
+                var path = Path.Combine(uploadFolder, photo.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream);
+                    stream.Close();
+                }
+                bool isBlurry = ImageProcessingService.IsBlurry(path);
+            }
+            catch (Exception ex)
+            {
+
+            }
+           
+            return Ok();
+        }
+
+
+    }
+}
---
+++ OpenCVSharp-dotnet6-docker/Services/ImageProcessingService.cs
@@ -0,0 +1,28 @@
+﻿using OpenCvSharp;
+
+namespace OpenCVSharp_dotnet6_docker.Services
+{
+    public static class ImageProcessingService
+    {
+        // as seen here: https://pyimagesearch.com/2015/09/07/blur-detection-with-opencv/
+        public static bool IsBlurry(string imageFilePath, double threshold = 100.0)
+        {
+            Mat mat = Cv2.ImRead(imageFilePath, ImreadModes.Grayscale);
+            var varianceOfLaplacian = VarianceOfLaplacian(mat);
+            return varianceOfLaplacian < threshold;
+        }
+
+        // as seen here: https://stackoverflow.com/questions/58005091/how-to-get-the-variance-of-laplacian-in-c-sharp
+        private static double VarianceOfLaplacian(Mat mat)
+        {
+            using var laplacian = new Mat();
+            int kernel_size = 3;
+            int scale = 1;
+            int delta = 0;
+            int ddepth = mat.Type().Depth;
+            Cv2.Laplacian(mat, laplacian, ddepth, kernel_size, scale, delta);
+            Cv2.MeanStdDev(laplacian, out var mean, out var stddev);
+            return stddev.Val0 * stddev.Val0;
+        }
+    }
+}
